dc8f4c34565f266a61dad62ce99c1d998c53d36a
Satish-Algo <satishkumarsingh2092003@gmail.com>
2026-04-10 10:12:21 +0530

    first commit

---
+++ e-commers/src/main/java/e_commers/com/example/e_commers/ECommersApplication.java
@@ -0,0 +1,14 @@
+package e_commers.com.example.e_commers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommersApplication.class, args);
+	}
+
+
+}
---
+++ e-commers/src/test/java/e_commers/com/example/e_commers/ECommersApplicationTests.java
@@ -0,0 +1,13 @@
+package e_commers.com.example.e_commers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
